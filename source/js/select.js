04a6ecba84ef9494ff46dfe2e90ae3e664b43bce
Rustam <rustam17.sb@gmail.com>
2024-03-16 00:43:20 +0300

    feat: add search to select cities

--- source/js/select.js
+++ source/js/select.js
@@ -1,8 +1,11 @@
 function initSelect(selectElement) {
   new Choices(selectElement, { // eslint-disable-line
-    searchEnabled: false,
+    searchEnabled: true,
+    noResultsText: 'Город не найден',
+    searchPlaceholderValue: 'Начните вводить название города',
     itemSelectText: '',
     shouldSort: false,
+    maxItemCount: 4,
     // placeholder: true,
     // placeholderValue: 'fff',
   });
