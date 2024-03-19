f9bd4a1c4bdf360df1415adea45468aaa5ef0fb4
Rustam <rustam17.sb@gmail.com>
2024-03-19 20:11:14 +0300

    fix: fix some Redmi Pad SE bugs - 2

--- source/js/cart-form.js
+++ source/js/cart-form.js
@@ -2,7 +2,7 @@ function initCartForm(formElement) {
   const promocodeElement = formElement.querySelector('.cart-form__promocode');
   const promocodeToggleButtonElement = formElement.querySelector('.cart-form__promocode-button');
 
-  promocodeToggleButtonElement.addEventListener('click', () => {
+  promocodeToggleButtonElement?.addEventListener('click', () => {
     promocodeElement.classList.toggle('cart-form__promocode--hidden');
   });
 }
