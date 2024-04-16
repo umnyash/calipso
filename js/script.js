350c0500c82270b07d25ef1d2ca943870a2b9eb6
umnyash <63366308+umnyash@users.noreply.github.com>
2024-04-16 02:43:25 +0000

    Deploying to gh-pages from @ umnyash/calipso@3646295db1c247e37e9ef03fcb777e869ec34d3d 🚀

--- js/script.js
+++ js/script.js
@@ -1437,30 +1437,27 @@ async function initMap(mapElement) {
 /* * * * * * * * * * * * * * * * * * * * * * * *
  * modal.js
  */
-const MODAL_APPEARANCE_TIME = 300;
 const MODAL_DISAPPEARANCE_TIME = 100;
 const openedModals = [];
 function openModal(modal) {
-  modal.classList.add('modal--open');
-  modal.classList.add('modal--opening');
+  modal.classList.remove('modal--hidden');
   openedModals.push(modal);
   document.addEventListener('keydown', onModalEscapeEvent);
   document.addEventListener('click', onModalClick);
   setTimeout(() => {
-    modal.classList.remove('modal--opening');
-  }, MODAL_APPEARANCE_TIME);
+    modal.classList.add('modal--open');
+  }, 100);
 }
 function closeModal(modal) {
   modal.classList.remove('modal--error');
-  modal.classList.add('modal--closing');
+  modal.classList.remove('modal--open');
   openedModals.pop();
   if (!openedModals.length) {
     document.removeEventListener('keydown', onModalEscapeEvent);
     document.removeEventListener('click', onModalClick);
   }
   setTimeout(() => {
-    modal.classList.remove('modal--open');
-    modal.classList.remove('modal--closing');
+    modal.classList.add('modal--hidden');
     if (modal.classList.contains('modal--with_alert') || modal.classList.contains('modal--with_review-gallery')) {
       modal.remove();
     }
