971dc45f952be5ed9151aa3545081ac5e2b5f964
umnyash <63366308+umnyash@users.noreply.github.com>
2024-05-23 17:00:39 +0000

    Deploying to gh-pages from @ umnyash/calipso@96b40e88d10ede3a0d6397cd0bc9cf15452844d5 🚀

--- js/script.js
+++ js/script.js
@@ -195,11 +195,54 @@ function createArticlePreviewTemplate(article, modificators) {
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
 /* * * * * * * * * * * * * * * * * * * * * * * *
+ * articles-slider.js
+ */
+function initArticlesSlider(articlesSliderElement) {
+  const swiperElement = articlesSliderElement.querySelector('.articles__slider');
+  const prevButtonElement = articlesSliderElement.querySelector('.articles__slider-arrows .slider-arrows__button--prev');
+  const nextButtonElement = articlesSliderElement.querySelector('.articles__slider-arrows .slider-arrows__button--next');
+  const swiper = new Swiper(swiperElement, {
+    slidesPerView: 'auto',
+    spaceBetween: 15,
+    navigation: {
+      prevEl: prevButtonElement,
+      nextEl: nextButtonElement
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      },
+      1346: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      },
+      1900: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
+  });
+  const filterElement = articlesSliderElement.querySelector('.articles__filter');
+  const slideElements = articlesSliderElement.querySelectorAll('.articles__slider-item');
+  const onFilterChange = evt => {
+    const tag = evt.target.value;
+    slideElements.forEach(slideElement => {
+      slideElement.classList.toggle('articles__slider-item--hidden', !(slideElement.dataset.tag === tag || tag === 'all'));
+    });
+    swiper.update();
+  };
+  filterElement.addEventListener('change', onFilterChange);
+}
+/* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
  * banners.js
  */
 function initBanners(bannersElement) {
   const sliderElement = bannersElement.querySelector('.banners__slider');
   const thumbnailsSliderElement = bannersElement.querySelector('.banners__thumbnails-slider');
+  const thumbnailElements = thumbnailsSliderElement.querySelectorAll('.banners__thumbnail');
   const thumbnailsSwiper = new Swiper(thumbnailsSliderElement, {
     spaceBetween: 5,
     slidesPerView: 'auto',
@@ -210,10 +253,20 @@ function initBanners(bannersElement) {
     fadeEffect: {
       crossFade: true
     },
+    loop: true,
+    autoplay: {
+      delay: 6000,
+      disableOnInteraction: false
+    },
     thumbs: {
       swiper: thumbnailsSwiper,
       slideThumbActiveClass: 'banners__thumbnails-item--active'
     }
+  });
+  bannersSwiper.on('autoplayTimeLeft', (_s, _time, progress) => {
+    thumbnailElements.forEach(thumbnailElement => {
+      thumbnailElement.style.setProperty('--thumbnail-progress', 1 - progress);
+    });
   });
 }
 /* * * * * * * * * * * * * * * * * * * * * * * */
@@ -3551,6 +3604,7 @@ document.querySelectorAll('.banners').forEach(initBanners);
 document.querySelectorAll('.map').forEach(initMap);
 document.querySelectorAll('.product-card').forEach(initProductCard);
 document.querySelectorAll('.products').forEach(initProducts);
+document.querySelectorAll('.articles--with-slider').forEach(initArticlesSlider);
 document.querySelectorAll('.navigation-shortcuts').forEach(initNavigationShortcuts);
 document.querySelectorAll('.catalog-sorting').forEach(initCatalogSorting);
 document.querySelectorAll('.product-images').forEach(initProductImages);
