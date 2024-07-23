9f6b86961e4ce6c5594476b9fbaab404bc9d08dd
Rustam Sadreev <rustam17.sb@gmail.com>
2024-07-23 20:33:47 +0300

    implement text copying

--- source/js/modules/document-modal.js
+++ source/js/modules/document-modal.js
@@ -3,6 +3,20 @@
  */
 function initDocumentModal(modalElement, openModal) {
   const modalName = modalElement.dataset.modal;
+  const headingElement = modalElement.querySelector('.document-modal__heading');
+  const contentElement = modalElement.querySelector('.document-modal__scroll-container-content');
+  const copyButtonElement = modalElement.querySelector('.document-actions__button--copy');
+
+  if (headingElement && contentElement && copyButtonElement) {
+    copyButtonElement.addEventListener('click', (evt) => {
+      evt.preventDefault();
+
+      const headingText = headingElement.textContent;
+      const contentText = contentElement.textContent;
+      const textToCopy = `${headingText} ${contentText}`;
+      navigator.clipboard.writeText(textToCopy);
+    });
+  }
 
   document.querySelectorAll(`[data-modal-opener="${modalName}"]`).forEach((openerElement) => {
     openerElement.addEventListener('click', (evt) => {
